1cff0673732bf8569247285316e209ceb81a469a
ffernandesx <48631210+ffernandesx@users.noreply.github.com>
2020-10-17 15:21:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,12 @@
 include('config.php');
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
-        <title>Members Area</title>
-    </head>
-    <body>
-    	<div class="header">
-        	<a href="<?php echo $url_home; ?>"><img src="<?php echo $design; ?>/images/logo.png" alt="Members Area" /></a>
-	    </div>
-        <div class="content">
+<html>
+<head>
+</head>
+<body>
 <?php
-//We display a welcome message, if the user is logged, we display it username
+
 ?>
 
 Hello
@@ -25,8 +17,7 @@ if(isset($_SESSION['username'])) {
 	echo ' '.htmlentities($_SESSION['username'], ENT_QUOTES, 'UTF-8');}
 ?>
 
-		<br />
-Welcome on our website.<br />
+<br />Welcome<br />
 <?php
 //If the user is logged, we display links to edit his infos, to see his pms and to log out
 if (isset($_SESSION['username'])) {
