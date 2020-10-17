e57dc7ce1d807ea40df7657eb4a8c1816b98e284
ffernandesx <48631210+ffernandesx@users.noreply.github.com>
2020-10-17 19:23:21 -0300

    Update new_pm.php

--- new_pm.php
+++ new_pm.php
@@ -9,36 +9,36 @@ include('config.php');
 	<body>
 
 <?php
-//We check if the user is logged
+//Check if the user is logged
 if (isset($_SESSION['username'])) {
 	$form     = true;
 	$otitle   = '';
 	$orecip   = '';
 	$omessage = '';
-	//We check if the form has been sent
+	//Check if the form has been sent
 	if (isset($_POST['title'], $_POST['recip'], $_POST['message'])) {
 		$otitle   = $_POST['title'];
 		$orecip   = $_POST['recip'];
 		$omessage = $_POST['message'];
-		//We remove slashes depending on the configuration
+		//Remove slashes depending on the configuration
 		if (get_magic_quotes_gpc()) {
 			$otitle   = stripslashes($otitle);
 			$orecip   = stripslashes($orecip);
 			$omessage = stripslashes($omessage);
 		}
-		//We check if all the fields are filled
+		//Check if all the fields are filled
 		if ($_POST['title'] != '' and $_POST['recip'] != '' and $_POST['message'] != '') {
-			//We protect the variables
+			//Protect the variables
 			$title   = mysqli_real_escape_string($link, $otitle);
 			$recip   = mysqli_real_escape_string($link, $orecip);
 			$message = mysqli_real_escape_string($link, nl2br(htmlentities($omessage, ENT_QUOTES, 'UTF-8')));
-			//We check if the recipient exists
+			//Check if the recipient exists
 			$dn1 = mysqli_fetch_array(mysqli_query($link, 'select count(id) as recip, id as recipid, (select count(*) from pm) as npm from users where username="'.$recip.'"'));
 			if ($dn1['recip'] == 1) {
-				//We check if the recipient is not the actual user
+				//Check if the recipient is not the actual user
 				if ($dn1['recipid'] != $_SESSION['userid']) {
 					$id = $dn1['npm']+1;
-					//We encrypt then send the message
+					//Encrypt then send the message
 					$cipher = "aes-128-gcm";
 					$ivlen  = openssl_cipher_iv_length($cipher);
 					$iv     = openssl_random_pseudo_bytes($ivlen);
@@ -71,10 +71,10 @@ if (isset($_SESSION['username'])) {
 	elseif (isset($_GET['recip'])) $orecip = $_GET['recip'];//We get the username for the recipient if available
 
 	if ($form) {
-		//We display a message if necessary
+		//Display a message if necessary
 		if (isset($error)) echo '<div class="message">'.$error.'</div>';
 
-		//We display the form
+		//Display the form
 ?>
 		<div class="content">
 			<h1>New Personnal Message</h1>
