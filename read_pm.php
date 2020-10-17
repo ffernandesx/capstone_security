3e86221eb944bffac81e6460f841622764797207
ffernandesx <48631210+ffernandesx@users.noreply.github.com>
2020-10-17 16:04:03 -0300

    Update read_pm.php

--- read_pm.php
+++ read_pm.php
@@ -2,18 +2,11 @@
 include('config.php');
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
 		<title>Read a PM</title>
 	</head>
 	<body>
-		<div class="header">
-			<a href="<?php echo $url_home; ?>"><img src="<?php echo $design; ?>/images/logo.png" alt="Members Area" /></a>
-		</div>
-
 <?php
 //We check if the user is logged
 if (isset($_SESSION['username'])) {
