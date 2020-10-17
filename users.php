01c28c7953caabc95e70e41015673a968efda919
ffernandesx <48631210+ffernandesx@users.noreply.github.com>
2020-10-17 18:49:43 -0300

    Update users.php

--- users.php
+++ users.php
@@ -3,17 +3,11 @@
 include('config.php');
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
 		<title>List of users</title>
 	</head>
 	<body>
-		<div class="header">
-			<a href="<?php echo $url_home; ?>"><img src="<?php echo $design; ?>/images/logo.png" alt="Members Area" /></a>
-		</div>
 		<div class="content">
 This is the list of members:
 			<table>
